f6ee02cc671690dc1a2c106a9e5e933862208a1a
JHud <219741840+JamHudson@users.noreply.github.com>
2026-04-25 19:41:36 -0700

    Wave 2

--- src/LowercaseSentenceTokenizerTest.java
+++ src/LowercaseSentenceTokenizerTest.java
@@ -16,9 +16,14 @@ class LowercaseSentenceTokenizerTest {
     }
 
     // Wave 2
-    /*
-     * Write your test here!
-     */
+    @Test
+    void testTokenizeWithManySpaces() {
+        LowercaseSentenceTokenizer tokenizer = new LowercaseSentenceTokenizer();
+        Scanner scanner = new Scanner("i     have too      many    spaces here and there    and kinda           um everywhere");
+        List<String> tokens = tokenizer.tokenize(scanner);
+
+        assertEquals(List.of("i", "have", "too", "many", "spaces", "here", "and", "there", "and", "kinda", "um", "everywhere"), tokens);
+    }
     
 
     // Wave 3
