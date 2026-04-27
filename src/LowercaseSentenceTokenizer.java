e6bff55823440d8f05bf1dfb4326efbfe53d1b38
Nadia <239692631+nadia5129@users.noreply.github.com>
2026-04-26 23:10:28 -0700

    created a list for tokenize

--- src/LowercaseSentenceTokenizer.java
+++ src/LowercaseSentenceTokenizer.java
@@ -30,6 +30,8 @@ public class LowercaseSentenceTokenizer implements Tokenizer {
    */
   public List<String> tokenize(Scanner scanner) {
     // TODO: Implement this function to convert the scanner's input to a list of words and periods
+    ArrayList<String> inputList = new ArrayList<String> ();
+
     return null;
   }
 }
