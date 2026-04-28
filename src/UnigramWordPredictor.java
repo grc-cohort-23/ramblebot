cd76ecde7d4bee203977869ceb494d49507b31a3
Nadia <239692631+nadia5129@users.noreply.github.com>
2026-04-27 20:25:58 -0700

    imported random

--- src/UnigramWordPredictor.java
+++ src/UnigramWordPredictor.java
@@ -3,6 +3,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
+import java.util.Random;
 
 /**
  * A class for predicting the next word in a sequence using a unigram model.
