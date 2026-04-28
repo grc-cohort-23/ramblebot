9e04ecf321736b6c94d6c6f83937345b6c681a98
jaronie <[200~Jisaa@BALONEY MINGW64 ~$ git config --global user.name jaronieJisaa@BALONEY MINGW64 ~$ git config --global user.email>
2026-04-27 22:15:55 -0700

    Skeleton for tokenize method started

--- src/LowercaseSentenceTokenizer.java
+++ src/LowercaseSentenceTokenizer.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
@@ -30,7 +31,10 @@ public class LowercaseSentenceTokenizer implements Tokenizer {
    */
   public List<String> tokenize(Scanner scanner) {
     // TODO: Implement this function to convert the scanner's input to a list of words and periods
-    return null;
+    ArrayList<String> words = new ArrayList<>();
+
+    return words;
+  
   }
 }
 
